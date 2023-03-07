053fb3d0b7240ba5d518fe713b701647e9370421
ivanroscuenca <ivanros@protonmail.com>
2023-03-07 19:37:22 +0100

    updated with Nivell 2 ex1

---
+++ src/N2ex1/Person.java
@@ -0,0 +1,44 @@
+package N2ex1;
+@JsonSerializable(directory = "/home/ivan/Escritorio/Tasca7/src/N2ex1/")
+public class Person {
+
+    @JsonElement(key="name")
+    private String firstName;
+
+    private String lastName;
+
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    // Standard getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
